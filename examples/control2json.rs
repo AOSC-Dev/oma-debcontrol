c7c91339aac8063a07ce87ad95250f75b0c43689
Felix Krull <f_krull@gmx.de>
2020-02-08 01:54:46 +0100

    Add example

---
+++ examples/control2json.rs
@@ -0,0 +1,33 @@
+use anyhow::{anyhow, Context};
+use debcontrol::{parse, Paragraph};
+use json::JsonValue;
+use std::{env::args_os, fs::read_to_string, io::stdout, path::PathBuf};
+
+fn paragraph_to_json(paragraph: Paragraph) -> JsonValue {
+    let mut obj = JsonValue::new_object();
+    for field in paragraph.fields {
+        obj.insert(field.name, JsonValue::String(field.value))
+            .unwrap();
+    }
+    obj
+}
+
+fn main() -> anyhow::Result<()> {
+    let filename: PathBuf = args_os()
+        .skip(1)
+        .next()
+        .context("specify a file name")?
+        .into();
+
+    let input = read_to_string(filename)?;
+    let json: JsonValue = parse(&input)
+        .collect::<Result<Vec<_>, _>>()
+        .map_err(|x| anyhow!("{}", x))?
+        .into_iter()
+        .map(paragraph_to_json)
+        .collect::<Vec<_>>()
+        .into();
+
+    json.write_pretty(&mut stdout(), 2)?;
+    Ok(())
+}
